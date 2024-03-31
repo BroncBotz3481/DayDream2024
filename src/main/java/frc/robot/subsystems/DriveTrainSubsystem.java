6a4af859e9235bba037bb508e50411a4d048c67c
Nobody <nobodyhere618@gmail.com>
2024-03-31 11:11:31 -0500

    Subsystem?
    Think im going somewhere

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,9 +7,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+
+public class DriveTrainSubsystem extends SubsystemBase {
+
+  CANSparkMax backLeftMotor = new CANSparkMax();
+
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public DriveTrainSubsystem() {}
 
   /**
    * Example command factory method.
@@ -40,8 +44,4 @@ public class ExampleSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
